23d3e29e45062556b58d0d5d1ae06ebd024db290
Nick Wu <nick@Chuntings-MacBook-Pro.local>
2017-10-09 12:27:10 +0800

    gg

--- HW1/test.py
+++ HW1/test.py
@@ -1,6 +1,10 @@
 from Utils.map_reader import phone_map_reader, phone_char_reader
+from Utils.load_data import load_batched_data
+import tensorflow
 
 path_to_phone_map = './data/phones/48_39.map'
 path_to_phone_char_map = './data/48phone_char.map'
+
+path_to_data_dir = './data/'
 
 print(phone_char_reader(path_to_phone_char_map))
