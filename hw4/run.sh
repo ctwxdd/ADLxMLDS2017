d98fecb80283b8d30a81cd2393c780c37349620f
ctwxdd <ctwxdd@gmail.com>
2018-01-05 21:57:06 +0800

    add -x

--- hw4/run.sh
+++ hw4/run.sh
@@ -2,4 +2,4 @@
 wget https://www.dropbox.com/s/v43blldpyg7yx7f/model-350500.data-00000-of-00001?dl=1 -O ./ckpt/model-350500.data-00000-of-00001
 wget https://www.dropbox.com/s/wupexxm1986v1ah/model-350500.index?dl=1 -O ./ckpt/model-350500.index
 wget https://www.dropbox.com/s/04clowwjp5c4y1t/model-350500.meta?dl=1 -O ./ckpt/model-350500.meta
-python generate.py -t $1
+python3 generate.py -t $1
