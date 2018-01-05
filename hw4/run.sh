3156251459c88d0a62033e38ba3ec7339edd3497
nick wu <nickwu9146@gmail.com>
2018-01-05 20:59:04 +0800

    fix

--- hw4/run.sh
+++ hw4/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget https://www.dropbox.com/s/v43blldpyg7yx7f/model-350500.data-00000-of-00001?dl=1 -P ./ckpt/ -O model-350500.data-00000-of-00001
-wget https://www.dropbox.com/s/wupexxm1986v1ah/model-350500.index?dl=1 -P ./ckpt/ -O model-350500.index
-wget https://www.dropbox.com/s/04clowwjp5c4y1t/model-350500.meta?dl=1 -P ./ckpt/ -O model-350500.meta
+wget https://www.dropbox.com/s/v43blldpyg7yx7f/model-350500.data-00000-of-00001?dl=1 -O ./ckpt/model-350500.data-00000-of-00001
+wget https://www.dropbox.com/s/wupexxm1986v1ah/model-350500.index?dl=1 -O ./ckpt/model-350500.index
+wget https://www.dropbox.com/s/04clowwjp5c4y1t/model-350500.meta?dl=1 -O ./ckpt/model-350500.meta
 python generate.py -t $1
